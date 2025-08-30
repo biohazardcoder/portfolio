a1c7f18f7edc3ceaa1e150eff357d9ef663c5896
Husanbek <buxorojahon@gmail.com>
2025-08-30 17:11:17 +0500

    W

--- client/src/middlewares/Fetch.tsx
+++ client/src/middlewares/Fetch.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 export const Fetch = axios.create({
-  baseURL: "http://portfolio.mukammal-crm.uz/api/",
+  baseURL: "https://portfolio.mukammal-crm.uz/api/",
   headers: {
     Authorization: token,
   },
